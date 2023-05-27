75c95cb20a5f7d3ef998c86e39ccd28924033fa2
jonathanforking <jonathanforking@proton.me>
2023-05-27 21:04:51 +0200

    Fixed vertical fling + Decoupled vertical and horizontal thresholds

--- src/index.ts
+++ src/index.ts
@@ -30,23 +30,25 @@ export type UpdateCallback = (element: SwipeableHTMLElement, touchDistance: numb
 
 export type CompleteCallback = (element: SwipeableHTMLElement, touchDirection: Direction, gesture: Gesture) => any;
 
-// TODO:
-// - separate speed & distance values for horizontal/vertical
-
 export interface Options {
   /** What movements should be considered (default is Mode.HORIZONTAL) */
   mode: Mode,
-  /** Speed threshold (in screen % traveled per ms) that needs to be crossed immediately
-   * before ending touch for it to count as a 'fling' (default is 0.005 = 0.5%) */
-  flingSpeed: number,
-  /** Touch travel distance (in screen %) to count as a swipe (default is 0.5 = 50%) */
-  swipeDistance: number,
+  /** Horizontal speed threshold (in screen % traveled per ms) that needs to be crossed
+   * immediately before ending touch for it to count as a 'fling' (default is 0.0025 = 0.25%) */
+  flingSpeedHor: number,
+  /** Vertical speed threshold (in screen % traveled per ms) that needs to be crossed
+   * immediately before ending touch for it to count as a 'fling' (default is 0.001 = 0.1%) */
+  flingSpeedVer: number,
+  /** Horizontal touch travel distance (in screen %) to count as a swipe (default is 0.5 = 50%) */
+  swipeDistanceHor: number,
+  /** Vertical touch travel distance (in screen %) to count as a swipe (default is 0.5 = 50%) */
+  swipeDistanceVer: number,
   /** Milliseconds that need to elapse between touchMove updates (default is 5) */
   updateRate: number,
   /** Touch travel distance (in pixels) before locking to X axis / swipe (default is 0) */
-  horizontalLock: number,
+  lockHor: number,
   /** Touch travel distance (in pixels) before locking to Y axis / scroll (default is 0) */
-  verticalLock: number
+  lockVer: number
 }
 
 export interface SwipeableHTMLElement extends HTMLElement {
@@ -80,11 +82,13 @@ export function addFlingSwipe(
   element.swipeConfig = element.swipeConfig ?? {
     options: {
       mode: Mode.HORIZONTAL,
-      flingSpeed: 0.005,
-      swipeDistance: 0.5,
+      flingSpeedHor: 0.0025,
+      flingSpeedVer: 0.001,
+      swipeDistanceHor: 0.5,
+      swipeDistanceVer: 0.5,
       updateRate: 5,
-      horizontalLock: 0,
-      verticalLock: 0
+      lockHor: 0,
+      lockVer: 0
     },
     update: [],
     start: [],
@@ -207,10 +211,10 @@ function dragTouch(e: TouchEvent) {
     // const deg = (Math.atan2(movY, movX) * 180 / Math.PI + 360) % 360;
     const xAbsDist = Math.abs(xDist);
     const yAbsDist = Math.abs(yDist);
-    if (xAbsDist > yAbsDist && xAbsDist > options.horizontalLock) {
+    if (xAbsDist > yAbsDist && xAbsDist > options.lockHor) {
       processCallbacks(config.start, target, Movement.HORIZONTAL);
       progress.movement = Movement.HORIZONTAL;
-    } else if (yAbsDist > xAbsDist && yAbsDist > options.verticalLock) {
+    } else if (yAbsDist > xAbsDist && yAbsDist > options.lockVer) {
       processCallbacks(config.start, target, Movement.VERTICAL);
       progress.movement = Movement.VERTICAL;
     }
@@ -224,16 +228,18 @@ function dragTouch(e: TouchEvent) {
     if (timePassed < options.updateRate) {
       return;
     }
-    const newDist = !progress.movement  // <=> progress.movement === Movement.HORIZONTAL
+    const newDist = progress.movement === Movement.HORIZONTAL
       ? xDist / window.innerWidth
       : yDist / window.innerHeight;
     const speed = Math.abs(newDist - progress.distance) / timePassed;
-    if (speed >= options.flingSpeed) {
-      // Decide fling direction based on difference to last update (rather than total distance).
-      // Fling direction then has to match overall swipe direction in endTouch for it to be valid.
+    if (progress.movement === Movement.HORIZONTAL && speed >= options.flingSpeedHor) {
       progress.flingDirection = newDist > progress.distance
-        ? !progress.movement ? Direction.RIGHT : Direction.BOTTOM
-        : !progress.movement ? Direction.LEFT : Direction.TOP;
+        ? Direction.RIGHT
+        : Direction.LEFT;
+    } else if (progress.movement === Movement.VERTICAL && speed >= options.flingSpeedVer) {
+      progress.flingDirection = newDist > progress.distance
+        ? Direction.BOTTOM
+        : Direction.TOP;
     } else {
       progress.flingDirection = Direction.NONE;
     }
@@ -247,22 +253,33 @@ function endTouch(e: TouchEvent) {
   const target = e.currentTarget as SwipeableHTMLElement;
   const config = target.swipeConfig!;
   const progress = target.swipeProgress!;
-  const GREATER = !progress.movement ? Direction.RIGHT : Direction.BOTTOM;
-  const SMALLER = !progress.movement ? Direction.LEFT : Direction.TOP;
+  const GREATER = progress.movement === Movement.HORIZONTAL ? Direction.RIGHT : Direction.BOTTOM;
+  const SMALLER = progress.movement === Movement.HORIZONTAL ? Direction.LEFT : Direction.TOP;
   switch (progress.flingDirection) {
     case Direction.NONE:
     {
       // Check for swipe
-      if (progress.distance > config.options.swipeDistance) {
-        processCallbacks(config.complete, target, GREATER, Gesture.SWIPE);
-      } else if (progress.distance < -config.options.swipeDistance) {
-        processCallbacks(config.complete, target, SMALLER, Gesture.SWIPE);
-      } else {
-        processCallbacks(config.complete, target, Direction.NONE, Gesture.NONE);
+      if (progress.movement === Movement.HORIZONTAL) {
+        if (progress.distance > config.options.swipeDistanceHor) {
+          processCallbacks(config.complete, target, GREATER, Gesture.SWIPE);
+        } else if (progress.distance < -config.options.swipeDistanceHor) {
+          processCallbacks(config.complete, target, SMALLER, Gesture.SWIPE);
+        } else {
+          processCallbacks(config.complete, target, Direction.NONE, Gesture.NONE);
+        }
+      } else if (progress.movement === Movement.VERTICAL) {
+        if (progress.distance > config.options.swipeDistanceVer) {
+          processCallbacks(config.complete, target, GREATER, Gesture.SWIPE);
+        } else if (progress.distance < -config.options.swipeDistanceVer) {
+          processCallbacks(config.complete, target, SMALLER, Gesture.SWIPE);
+        } else {
+          processCallbacks(config.complete, target, Direction.NONE, Gesture.NONE);
+        }
       }
       break;
     }
     case Direction.RIGHT:
+    case Direction.BOTTOM:
     {
       if (progress.distance > 0) {
         processCallbacks(config.complete, target, GREATER, Gesture.FLING);
@@ -272,6 +289,7 @@ function endTouch(e: TouchEvent) {
       break;
     }
     case Direction.LEFT:
+    case Direction.TOP:
     {
       if (progress.distance < 0) {
         processCallbacks(config.complete, target, SMALLER, Gesture.FLING);
